fee484a849851e25bd0690090ba4e889a5b3f552
Windik007 <92020455+Windik007@users.noreply.github.com>
2021-10-27 17:02:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,4 @@ import discord
 from discord.ext import commands
 from config import settings
 
-bot = commands.Bot(command_prefix = settings['.'])
-
-@bot.command() 
-async def hello(ctx): 
-    author = ctx.message.author 
-
-    await ctx.send(f'Hello, {author.mention}!')
-
-    bot.run('OTAyOTEzNjIwODc3Mzk0MDEw.YXlV1g.TGTPnzAZxw_bdD_2XBV13nbhz1M')
+bot.run('OTAyOTEzNjIwODc3Mzk0MDEw.YXlV1g.TGTPnzAZxw_bdD_2XBV13nbhz1M')
